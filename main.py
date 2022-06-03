bfda8be923f321a47a3361a30a86d41645473193
justxd22 <xdjust18@gmail.com>
2022-06-03 11:09:03 +0300

    fix-flask

--- main.py
+++ main.py
@@ -5,4 +5,11 @@ bryll = Flask(__name__)
 @bryll.route("/fetch_url", methods =["GET", "POST"])
 def fetch_url():
     if request.method == "POST":
-        return render_template("index.html")
+        return render_template("index.htm")
+
+@bryll.route("/", methods =["GET"])
+def main():
+    return render_template("./index.htm")
+
+bryll.run()
+
